6d0b9a6034dd620ce88e262384a1c1b5b8d60348
Ludovic Maître <ludovic.maitre@effervens.com>
2017-09-03 13:41:31 +0200

    Check extension for upload type

--- bonita/commands/upload.py
+++ bonita/commands/upload.py
@@ -7,7 +7,21 @@ from .base import Base
 
 import os
 import requests
+from wheel.test.test_ranking import sup
 
+SUPPORTED_TYPES = {
+    'file': [],
+    'process': ['.bar'],
+    'organization': ['.xml'],
+    'actors': ['.xml'],
+    'image': [ '.png', '.jpg', '.gif', '.jpeg', '.bmp', '.wbmp', '.tga'],
+    'page': ['.zip'],
+    'applications': ['.xml'],
+    'connector' : ['.zip'],
+    'report': [],
+    'resource': ['.jar'],
+    'profiles': ['.xml']
+}
 
 class Upload(Base):
     """Manage uploads"""
@@ -16,29 +30,25 @@ class Upload(Base):
         #print('You supplied the following options:', dumps(self.options, indent=2, sort_keys=True))
         if self.options['upload']:
             self.upload()
-# /portal/fileUpload, supports any type of files
-# /portal/processUpload, supports only .bar files
-# /portal/organizationUpload, supports only .xml files
-# /portal/actorsUpload, supports only .xml files
-# /portal/imageUpload, supports only .png, .jpg, .gif, .jpeg, .bmp, .wbmp or .tga files
-# /portal/pageUpload, supports only .zip files
-# /portal/applicationsUpload, supports only .xml files
-# /portal/connectorImplementation, supports only .zip files (not available in Community edition)
-# /portal/reportUpload, supports any type of file (not available in Community edition)
-# /portal/resourceUpload, supports only .jar files (not available in Community edition)
-# /portal/profilesUpload, supports only .xml files (not available in Community edition)
 
     def upload(self):
-        type = self.options['<type>']
-        filename = self.options['<filename>']
+        uploadType = self.options['<type>']
+        if uploadType not in SUPPORTED_TYPES:
+            print('Unsupported upload type: %s' % uploadType)
+            return
+        filename,extension = os.path.splitext(self.options['<filename>'])
+        if len(SUPPORTED_TYPES.get(uploadType)) != 0 and not extension in SUPPORTED_TYPES.get(uploadType):
+            print('Unsupported extension %s for upload type %s (supported: %s)' % (extension, uploadType, SUPPORTED_TYPES.get(uploadType)))
+            return
         configuration = self.loadConfiguration()
         url = configuration['url']
         cookies = configuration['cookies']
 
-        files = {'file': open(filename, 'rb')}
-        r = requests.post(url + '/portal/' + type + 'Upload', files=files, cookies=cookies)
+        files = {'file': open(filename + extension, 'rb')}
+        r = requests.post(url + '/portal/' + uploadType + 'Upload', files=files, cookies=cookies)
         if r.status_code == 200:
             print('OK')
+            print(r.text)
         else:
             print('KO - %d' % r.status_code)
             print(r.text)
