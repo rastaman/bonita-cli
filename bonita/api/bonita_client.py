2e524acb6613d4528b1c7390fa8eb72b673be7cc
Ludovic Maître <ludovic.maitre@effervens.com>
2017-09-08 08:14:46 +0200

    Prepare for BDM API

--- bonita/api/bonita_client.py
+++ bonita/api/bonita_client.py
@@ -39,6 +39,27 @@ class BonitaClient:
   <null/>
 </object-stream>"""
 
+    XML_SESSION_TEMPLATE = """<object-stream>
+  <map>
+    <entry>
+      <string>session</string>
+      <org.bonitasoft.engine.session.impl.APISessionImpl>
+        <id>{}</id>
+        <creationDate>2017-09-07 09:45:39.844 UTC</creationDate>
+        <duration>3600000</duration>
+        <userName>{}</userName>
+        <userId>{}</userId>
+        <technicalUser>{}</technicalUser>
+        <tenantName>default</tenantName>
+        <tenantId>1</tenantId>
+      </org.bonitasoft.engine.session.impl.APISessionImpl>
+    </entry>
+  </map>
+</object-stream>"""
+
+    API_IDENTITY = "org.bonitasoft.engine.api.IdentityAPI"
+    API_BDM = "org.bonitasoft.engine.api.TenantAdministrationAPI"
+
     def __init__(self, configuration):
         self.configuration = configuration
         self.url = None
@@ -89,33 +110,11 @@ class BonitaClient:
 # "user_id": "-1", 
 # "copyright": "Bonitasoft \u00a9 2017", 
 # "is_technical_user": "true", 
-# "session_id": "-2557273944568136190", 
+# "session_id": "-2557273944136190", 
 # "version": "7.5.4", 
-# "conf": "[\"24BA6D78D000697A7E58F7B643DA29374D4BDCFA\",\"91C7296E408550048C52AD21719A4FA05F54F55E\",\"DD9C4052DBEC7A93BEFBBB57CFA3A6E51D6FCF1B\",\"4001F092E42A4017FED92758EF25627AEAF6E23C\",\"9002847FAED17E3A8CA4E5B53C4D28A3C9EBCAAD\",\"A745067BD67F498A466FC87BD13B57DD5B5DB6CE\",\"AAE57859FC0B7E133C547E7B2DCDF5C95D342C78\",\"C24B0B5D1808D6E55CEDD31A7F858C0295C5CB78\"]", 
+# "conf": "[\"24BA6D78D0007E58F7B643DA29374D4BDCFA\",\"91C7296E408550048C52AD21719A4F54F55E\",\"DD9C4052DBEC7A93BEFBBBA3A6E51D6FCF1B\",\"4001F092E42A4017FED92758EF25627AEAF6E23C\",\"9002847FAED17E3A8CA4E5B53C4D28A3C9EBCAAD\",\"A745067BD67F498A466FC87BD13B57DD5B5DB6CE\",\"AAE57859FC0B7E133C547E7B2DCDF5C95D342C78\",\"C24B0B5D1808D6E55CEDD31A7F8295C5CB78\"]", 
 # "user_name": "install"
 #}
-
-    XML_SESSION_TEMPLATE = """
-<object-stream>
-  <map>
-    <entry>
-      <string>session</string>
-      <org.bonitasoft.engine.session.impl.APISessionImpl>
-        <id>{}</id>
-        <creationDate>2017-09-07 09:45:39.844 UTC</creationDate>
-        <duration>3600000</duration>
-        <userName>{}</userName>
-        <userId>{}</userId>
-        <technicalUser>{}</technicalUser>
-        <tenantName>default</tenantName>
-        <tenantId>1</tenantId>
-      </org.bonitasoft.engine.session.impl.APISessionImpl>
-    </entry>
-  </map>
-</object-stream>
-"""
-
-
 
     def xmlSessionFromSession(self, session):
         xmlSession = BonitaClient.XML_SESSION_TEMPLATE.format(
@@ -277,7 +276,7 @@ class BonitaClient:
         rc, raw_session = self.getSession()
         session = json.loads(raw_session)
         xmlSession = self.xmlSessionFromSession(session)
-        url  = self.url + "/serverAPI/" + "org.bonitasoft.engine.api.IdentityAPI" + "/" + "importOrganization"
+        url  = self.url + "/serverAPI/" + BonitaClient.API_IDENTITY + "/" + "importOrganization"
         headers = { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' }
         with open(organizationFilename, 'r') as organizationFile:
             data = organizationFile.read()
@@ -300,21 +299,89 @@ class BonitaClient:
             "classNameParameters": BonitaClient.CLASSNAME_VOID_PARAMETER,
             "parametersValues": BonitaClient.VALUE_NULL_PARAMETER
         }
-        url  = self.url + "/serverAPI/" + "org.bonitasoft.engine.api.IdentityAPI" + "/" + "deleteOrganization"
+        url  = self.url + "/serverAPI/" + BonitaClient.API_IDENTITY + "/" + "deleteOrganization"
         headers = { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' }
         r = self.getInternalSession().post(url, data=payload, headers=headers)
         return self.formatResponse(r)
 
     def exportOrganization(self):
         rc, raw_session = self.getSession()
-        session = json.loads(raw_session)
-        xmlSession = self.xmlSessionFromSession(session)
+        xmlSession = self.xmlSessionFromSession(json.loads(raw_session))
         payload = {
             "options": xmlSession,
             "classNameParameters": BonitaClient.CLASSNAME_VOID_PARAMETER,
             "parametersValues": BonitaClient.VALUE_NULL_PARAMETER
         }
-        url  = self.url + "/serverAPI/" + "org.bonitasoft.engine.api.IdentityAPI" + "/" + "exportOrganization"
+        url  = self.url + "/serverAPI/" + BonitaClient.API_IDENTITY + "/" + "exportOrganization"
         headers = { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' }
         r = self.getInternalSession().post(url, data=payload, headers=headers)
         return self.formatResponse(r)
+
+    # BDM API - Tenant Administration API  
+    # Pause the tenant and restart it before use
+
+    def installBusinessDataModel(self, zipFilename):
+        rc, raw_session = self.getSession()
+        session = json.loads(raw_session)
+        xmlSession = self.xmlSessionFromSession(session)
+        url  = self.url + "/serverAPI/" + BonitaClient.API_BDM + "/" + "installBusinessDataModel"
+        headers = { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' }
+        with open(zipFilename, 'rb') as organizationFile:
+            data = organizationFile.read()
+            escapedData = self.escapeXml(data)
+            payload = {
+                "options": xmlSession,
+                "classNameParameters": BonitaClient.CLASSNAME_STRING_PARAMETER, 
+                "parametersValues": BonitaClient.VALUE_STRING_PARAMETER.format( escapedData )
+            }
+            r = self.getInternalSession().post(url, data=payload, headers=headers)
+            return self.formatResponse(r)
+        return 'KO'
+        
+        return
+#String installBusinessDataModel(byte[] zip)
+#                         throws InvalidBusinessDataModelException,
+#                                BusinessDataRepositoryDeploymentException
+#Installs a new business data model.
+#Parameters:
+#zip - the binary content of the business object model.
+#Returns:
+#the version of the Business Data Model just deployed.
+#Throws:
+#InvalidBusinessDataModelException - if the Business Data Model content passed as parameter is invalid.
+#BusinessDataRepositoryDeploymentException - if the deployment cannot be fulfilled completely.
+    
+    def uninstallBusinessDataModel(self):
+        return
+#void uninstallBusinessDataModel()
+#                         throws BusinessDataRepositoryDeploymentException
+#Uninstalls the business data model.
+#Throws:
+#BusinessDataRepositoryDeploymentException - if the deployment cannot be fulfilled completely.
+
+    def cleanAndUninstallBusinessDataModel(self):
+        return
+#void cleanAndUninstallBusinessDataModel()
+#                                 throws BusinessDataRepositoryDeploymentException
+#Deletes all business data and uninstalls the business data model.
+#Throws:
+#BusinessDataRepositoryDeploymentException - if the deployment cannot be fulfilled completely.
+
+    def getClientBDMZip(self):
+        return
+#byte[] getClientBDMZip()
+#                throws BusinessDataRepositoryException
+#Returns:
+#zip content of the deployed client Business data model, null if no Business data model has been deployed
+#Throws:
+#BusinessDataRepositoryException - if the Business Data Model cannot be retrieved.
+
+    def getBusinessDataModelVersion(self):
+        return
+#String getBusinessDataModelVersion()
+#                            throws BusinessDataRepositoryException
+#Returns the current Business Data Model version, if any, or null if no Business Data Model is currently deployed.
+#Returns:
+#the current Business Data Model version, if any, or null if no Business Data Model is currently deployed
+#Throws:
+#BusinessDataRepositoryException - if the BDM version cannot be retrieved properly.        
---
+++ docs/TODOS.md
@@ -0,0 +1,13 @@
+# Todos
+
+## Tenants
+
+It would be nice to support tenants where we use the 1/default tenant.
+
+## Full server sessions
+
+It would be nice to fill the fields which are not templates in the xml server session by real values, or have explanations about what is their purpose.
+
+## Use a proper xml escaping function
+
+Currently we use a handmade escape xml function, it would be better to have a tested one.
